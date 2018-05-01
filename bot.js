1e0f235b8957b44b0460ccc5ed4beb3fb67dd472
Tonni Tielens <tonnitielens@gmail.com>
2018-05-01 15:54:53 +0200

    Send welcoming message

--- bot.js
+++ bot.js
@@ -5,10 +5,22 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+// Create an event listener for messages that contain 'ping'
 client.on('message', message => {
     if (message.content === 'ping') {
+        // Reply with 'pong'
         message.reply('pong');
     }
+});
+
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.find('name', 'member-log');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    // Send the message, mentioning the member
+    channel.send(`Hey ${member}, welcome to Sioux Gamers :tada::hugging: ! Please note, that although this server is intended for Sioux employees and their friends, it is a public server and not the place to discuss confidential information.`);
 });
 
 client.login(process.env.BOT_TOKEN);
