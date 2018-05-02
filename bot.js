4222ad06b11a3c8153782cfad8a1313700f89322
Tonni Tielens <tonnitielens@gmail.com>
2018-05-02 18:08:27 +0200

    Make that user is mentioned when kick is used

--- bot.js
+++ bot.js
@@ -30,6 +30,9 @@ client.on('message', message => {
     
         message.channel.send(`First argument: ${args[0]}`);
     } else if (command === 'kick') {
+        if (!message.mentions.users.size) {
+            return message.reply('you need to tag a user in order to kick them!');
+        }
         // grab the "first" mentioned user from the message
         // this will return a `User` object, just like `message.author`
         const taggedUser = message.mentions.users.first();
