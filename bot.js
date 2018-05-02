c583bb449d84114f5949b7f4eef95cda088de6d3
Tonni Tielens <tonnitielens@gmail.com>
2018-05-02 17:41:46 +0200

    Splits args and command

--- bot.js
+++ bot.js
@@ -7,6 +7,11 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+
     if (message.content.startsWith(`${prefix}ping`)) {
         message.channel.send('Pong.');
     } else if (message.content.startsWith(`${prefix}beep`)) {
@@ -15,6 +20,12 @@ client.on('message', message => {
         message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
     } else if (message.content === `${prefix}user-info`) {
         message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+    } else if (command === 'args-info') {
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+    
+        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
     }
 });
 
