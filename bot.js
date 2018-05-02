222f874f1548a9817979eae181dab8ce022e1f2d
Tonni Tielens <tonnitielens@gmail.com>
2018-05-02 19:15:31 +0200

    Fix reply

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ client.on('message', message => {
         message.channel.send(`First argument: ${args[0]}`);
     } else if (command === 'kick') {
         if (!message.mentions.users.size) {
-            return message.reply('you need to tag a user in order to kick them!');
+            return message.channel.send('you need to tag a user in order to kick them!');
         }
         // grab the "first" mentioned user from the message
         // this will return a `User` object, just like `message.author`
