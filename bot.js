5aa519a07a36d6ddf6d7ccd7e1243fd29703b6cd
Tonni Tielens <tonnitielens@gmail.com>
2018-05-02 17:59:32 +0200

    Accept argument foo

--- bot.js
+++ bot.js
@@ -24,8 +24,11 @@ client.on('message', message => {
         if (!args.length) {
             return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
         }
+        else if (args[0] === 'foo') {
+            return message.channel.send('bar');
+        }
     
-        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+        message.channel.send(`First argument: ${args[0]}`);
     }
 });
 
